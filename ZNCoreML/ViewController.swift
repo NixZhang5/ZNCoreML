9fa3561b42067f098462c6369b66b4a4360f6a67
xinpin <351235445@qq.com>
2018-11-30 10:15:51 +0800

    add vision prediction

--- ZNCoreML/ViewController.swift
+++ ZNCoreML/ViewController.swift
@@ -8,21 +8,22 @@
 
 import UIKit
 import CoreML
+import Vision
 
 class ViewController: UIViewController, UINavigationControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var classifier: UILabel!
-    var model: Inceptionv3!
+    @IBOutlet weak var visionClassifier: UILabel!
+    
+    lazy var model: Inceptionv3 = {
+        var model = Inceptionv3()
+        return model
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        model = Inceptionv3()
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,9 +32,7 @@ class ViewController: UIViewController, UINavigationControllerDelegate {
     }
 
     @IBAction func camera(_ sender: Any) {
-        if !UIImagePickerController.isSourceTypeAvailable(.camera) {
-            return
-        }
+        if !UIImagePickerController.isSourceTypeAvailable(.camera) { return }
         
         let cameraPicker = UIImagePickerController()
         cameraPicker.delegate = self
@@ -49,6 +48,49 @@ class ViewController: UIViewController, UINavigationControllerDelegate {
         picker.sourceType = .photoLibrary
         present(picker, animated: true, completion: nil)
     }
+    
+    func coreMLPredictionWithInceptionv3FromImage(_ image: UIImage) {
+        
+        UIGraphicsBeginImageContextWithOptions(CGSize(width: 299, height: 299), true, 2.0)
+        image.draw(in: CGRect(x: 0, y: 0, width: 299, height: 299))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        
+        guard let prediction = try? model.prediction(image: newImage.buffer()!) else { return }
+        classifier.text = "ML 识别结果:\(prediction.classLabel), 匹配率:\(prediction.classLabelProbs[prediction.classLabel] ?? 0)."
+    }
+    
+    func visionPredictionWithInceptionv3FromImage(_ image: UIImage) {
+        
+        let cgImage = image.cgImage!
+        var vnCoreMLModel: VNCoreMLModel!
+        do {
+            vnCoreMLModel = try VNCoreMLModel(for: model.model)
+        } catch {
+            print(error)
+            return
+        }
+        
+        let handle = VNImageRequestHandler.init(cgImage: cgImage, options: [:])
+        let request = VNCoreMLRequest.init(model: vnCoreMLModel) { [weak self] (request, error) in
+            var confidence: Float = 0.0
+            var tempClassification: VNClassificationObservation?
+            for classification in request.results! {
+                guard let cf = classification as? VNClassificationObservation else { continue }
+                if (cf.confidence > confidence) {
+                    confidence = cf.confidence
+                    tempClassification = cf
+                }
+            }
+            self?.visionClassifier.text = "Vision 识别结果:\(tempClassification?.identifier ?? "None"), 匹配率:\(tempClassification?.confidence ?? 0)"
+        }
+        
+        do {
+            try handle.perform([request])
+        } catch {
+            print(error)
+        }
+    }
 }
 
 
@@ -61,20 +103,14 @@ extension ViewController: UIImagePickerControllerDelegate {
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         
         picker.dismiss(animated: true, completion: nil)
-        
-        classifier.text = "Analyzing Image..."
-        guard let image = info["UIImagePickerControllerOriginalImage"] as? UIImage else { return }
-        UIGraphicsBeginImageContextWithOptions(CGSize(width: 299, height: 299), true, 2.0)
-        image.draw(in: CGRect(x: 0, y: 0, width: 299, height: 299))
-        let newImage = UIGraphicsGetImageFromCurrentImageContext()!
-        UIGraphicsEndImageContext()
+        guard let image = info[UIImagePickerControllerEditedImage] as? UIImage else { return }
+        imageView.image = image
 
-        imageView.image = newImage
-        
-        guard let prediction = try? model.prediction(image: newImage.buffer()!) else {
-            return
-        }
-        classifier.text = "I think this is a \(prediction.classLabel)."
+        classifier.text = "ML: Analyzing Image..."
+        coreMLPredictionWithInceptionv3FromImage(image)
+
+        visionClassifier.text = "Vision: Analyzing Image..."
+        visionPredictionWithInceptionv3FromImage(image)
     }
 }
 
